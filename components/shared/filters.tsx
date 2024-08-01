2be71c397e2b679adc689191689add6546455a41
ProxxTeabag <teabagproxx@gmail.com>
2024-08-01 05:50:55 +0300

    in progress

---
+++ components/shared/filters.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Title } from './title';
+import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui';
+import { RangeSlider } from './range-slider';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
+
+type Props = {
+  className?: string;
+};
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={className}>
+      <Title text="Filters" size="sm" className="mb-5 font-bold" />
+      {/* Верхние чекбоксы */}
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="Можно забрать" value={'1'} />
+        <FilterCheckbox text="Новинки" value={'2'} />
+      </div>
+      {/* Фильтр цен */}
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Price from & to</p>
+        <div className="flex gap-3 mb-5">
+          <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0} />
+          <Input type="number" min={100} max={1000} placeholder="1000" />
+        </div>
+        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
+      </div>
+      {/* Фильтр ингредиентов */}
+
+      <CheckboxFiltersGroup
+        title={'Contains'}
+        className="mt-5"
+        limit={6}
+        defaultItems={[
+          { text: 'Мясо', value: '1' },
+          { text: 'Грибы', value: '2' },
+          { text: 'Острое', value: '3' },
+          { text: 'Зелень', value: '4' },
+          { text: 'Чеснок', value: '5' },
+          { text: 'Помидоры', value: '6' },
+        ]}
+        items={[
+          { text: 'Мясо', value: '1' },
+          { text: 'Грибы', value: '2' },
+          { text: 'Острое', value: '3' },
+          { text: 'Зелень', value: '4' },
+          { text: 'Чеснок', value: '5' },
+          { text: 'Помидоры', value: '6' },
+          { text: 'Сыр', value: '7' },
+          { text: 'Оливки', value: '8' },
+          { text: 'Помидоры', value: '9' },
+          { text: 'Сыр', value: '10' },
+          { text: 'Оливки', value: '11' },
+          { text: 'Помидоры', value: '12' },
+        ]}
+      />
+    </div>
+  );
+};
